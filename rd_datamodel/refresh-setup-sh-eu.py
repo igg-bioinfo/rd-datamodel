56fe92e92a711dc77f4b44c6a4cd50ba71c7a483
ordet76 <robertocavanna@gaslini.org>
2022-12-16 11:19:38 +0100

    add set-up-eu.sh

---
+++ rd_datamodel/refresh-setup-sh-eu.py
@@ -0,0 +1,39 @@
+#'////////////////////////////////////////////////////////////////////////////
+#' FILE: refresh-setup-sh.py
+#' AUTHOR: David Ruvolo
+#' CREATED: 2022-02-07
+#' MODIFIED: 2022-02-07
+#' PURPOSE: Refresh setup.py script
+#' STATUS: stable
+#' PACKAGES: os, re
+#' COMMENTS: run `yarn m:update-setup 
+#'////////////////////////////////////////////////////////////////////////////
+
+from os import listdir
+import re
+
+dir = 'dist'
+dir_lu = 'lookups'
+file = 'setup-eu.sh'
+files = listdir(dir_lu)
+filepath = f'{file}'
+
+start = '# <!--- start: listEmxFiles --->\n'
+end = '# <!--- end: listEmxFiles --->\n'
+
+files = [
+    f'mcmd import -p {dir_lu}/{f}\n' for f in files
+    if re.search(r'(umdm_lookups_[_a-zA-Z]{1,}.csv)', f)
+]
+
+setupFiles = ['mcmd import -p dist/EUROFEVER.xlsx\n'] + files
+
+
+with open(filepath, 'r') as stream:
+    contents = stream.readlines()
+    
+newContents = contents[:contents.index(start) + 1] + setupFiles + contents[contents.index(end):]
+
+with open(filepath, 'w') as stream:
+    stream.writelines(newContents)
+
